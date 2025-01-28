552b285e1f47d0cde513c168025e2a9ebb77e035
pavancos <pavankc005@gmail.com>
2025-01-28 10:22:52 +0530

    fixes: card packs and auditrs

--- bin/card.js
+++ bin/card.js
@@ -11,11 +11,11 @@ const sections = [
   {
     title: 'Pavan Kumar Chennupati',
     details: [
-      [ 'Email', 'pavankc005@gmail.com' ],
-      [ 'Web', 'https://pavancos.github.io/pavan-info' ],
-      [ 'Twitter', 'https://x.com/pavancos' ],
-      [ 'GitHub', 'https://github.com/pavancos' ],
-      [ 'LinkedIn', 'https://linkedin.com/in/pavankch' ],
+      ['Email', 'pavankc005@gmail.com'],
+      ['Portfolio', 'https://pavanc.me'],
+      ['Twitter', 'https://x.com/pavancos'],
+      ['GitHub', 'https://github.com/pavancos'],
+      ['LinkedIn', 'https://linkedin.com/in/pavankch'],
     ],
   }
 ]
